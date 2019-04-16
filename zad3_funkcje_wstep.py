5941838dfe772ea67621f92c74f089280660bc27
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2019-04-16 22:24:43 +0200

    Create zad3_funkcje_wstep.py

---
+++ zad3_funkcje_wstep.py
@@ -0,0 +1,20 @@
+# Od razu z zadaniami :)
+# w książce od strony 37 do 40 znajduje się rozdział o funkcjach
+# przykłady dotyczą funkcji dodawania odejmowania mnożenia i dzielenia
+
+# a więc bazując stronach z ksiązki 
+# ponownie napiszcie kalkulator dla 10 dowolnych działań
+
+# poniżej pokażę przykład
+
+def dodawanie(a, b):
+  return a+b
+
+print(dodawanie(2,3))
+
+# powyższe jest  równoznaczne z:
+
+x = dodawanie(2,3)
+print(x)
+
+# powodzenia, w nagrodzę na następne zajęcia będą 4 płytki microbit :)
