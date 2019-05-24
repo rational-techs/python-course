32cd82992b7c5896968f28a07dc5b2512267c095
gorecki gracjan <gorecki.gracjan@hotmail.com>
2019-05-24 15:16:11 +0200

    for range

---
+++ zad5/petle.py
@@ -0,0 +1,20 @@
+# jak działam?
+# dzisiaj netypowe zajecia
+# nie będę tłumaczyć jak działają poniższe pętle
+# wy do tego dojdziecie :)
+
+
+
+
+
+# for i in range(0,100):
+#     print(i)
+
+
+
+# suma = 0
+# for i in range(0,10):
+#     suma = suma + i
+#     print(suma)
+
+
